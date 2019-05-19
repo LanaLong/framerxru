72bb5d2dc2d1c965a4640017f92d647d9e2be8a6
LanaLong <qmexperience@gmail.com>
2019-05-19 14:00:12 +0300

    framerx initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Framer X`,
+    description: `Framer X for Ru`,
+    author: `@lanlong`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Second page</h1>
+    {/* <p>second</p> */}
+    <Link to="/">Homepage</Link>
   </Layout>
 )
 
