7bba662e7ac824aaaa9e676b7aef2f4d7d48e94f
LanaLong <qmexperience@gmail.com>
2019-05-26 17:22:14 +0300

    test page-3

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,17 +12,20 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+const getSiteMetadata = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={getSiteMetadata}
     render={data => (
       <>
         <Header
@@ -30,12 +33,20 @@ const Layout = ({ children }) => (
           dragMomentum={false}
           siteTitle={data.site.siteMetadata.title}
         />
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built by{" "}
+            {data.site.siteMetadata.author}
+          </footer>
+        </div>
       </>
     )}
   />
---
+++ src/pages/page-3.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+
+export default () => (
+  <Layout>
+    <h1>Hello from page 3</h1>
+    <Link to="/">Home</Link>
+  </Layout>
+)
