f74069684481903986d1956ea6c9f7124ac65cc8
LanaLong <qmexperience@gmail.com>
2019-06-15 23:43:19 +0300

    1 минута, 5 минут

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -5,6 +5,15 @@ import Layout from "../components/layout"
 const PostTemplate = ({ data: post }) => (
   <Layout>
     <h1>{post.markdownRemark.frontmatter.title}</h1>
+    <h4>
+      Время чтения: {post.markdownRemark.timeToRead}{" "}
+      {/* {post.markdownRemark.timeToRead > 1 ? "минута" : "минут"} */}
+      {post.markdownRemark.timeToRead === 1
+        ? "минута"
+        : [2, 3, 4].includes(post.markdownRemark.timeToRead)
+        ? "минуты"
+        : "минут"}
+    </h4>
     <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }} />
   </Layout>
 )
@@ -12,6 +21,7 @@ const PostTemplate = ({ data: post }) => (
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      timeToRead
       html
       frontmatter {
         title
