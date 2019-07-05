f5fa637042d40fb5e5313615cc0c51c87845dda5
LanaLong <qmexperience@gmail.com>
2019-07-05 14:42:10 +0300

    need to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 // import Slider from "../components/slider"
-
+// let data = moment().format("MMMM Do YYYY, h:mm:ss a")
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>First Page</h1>
-
+    <h2>
+      Наблюдение за <span>временем</span>
+    </h2>
+    {/* <p>{data}</p> */}
     <Link to="/page-2/">Second page</Link>
   </Layout>
 )
